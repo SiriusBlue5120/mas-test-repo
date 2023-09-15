80698b4d05aa537fd84e5977186ad8c74e9187db
Shinas Shaji <80461084+SiriusBlue5120@users.noreply.github.com>
2023-09-15 15:54:11 +0200

    merge branch 'timing'

--- infLoop.py
+++ infLoop.py
@@ -1,9 +1,16 @@
+import time
+
 i = 0
 sums = 0
 
 while True:
+    startTime = time.time()
+    
     print(f"This is an infinite loop - we are now at iteration {i} and the sum of integers so far is {sums}")
     i = i + 1
     sums = sums + i
+    endTime = time.time()
+    
+    print(f"This iteration took {endTime - startTime}")
 
 print("This line will never print.")

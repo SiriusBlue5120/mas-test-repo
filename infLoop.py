37ed4ab1df580af75a499f63eb9350c04dc339df
Shinas Shaji <shinasshaji12@outlook.com>
2023-09-15 14:22:09 +0200

    Merged branch /sums to /main

--- infLoop.py
+++ infLoop.py
@@ -1,7 +1,9 @@
 i = 0
+sums = 0
 
 while True:
-    print(f"This is an infinite loop - we are now at iteration {i}")
+    print(f"This is an infinite loop - we are now at iteration {i} and the sum of integers so far is {sums}")
     i = i + 1
+    sums = sums + i
 
 print("This line will never print.")
